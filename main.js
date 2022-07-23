c488c82b8c07bdddd6c5d67c8780b81cbcd1e371
Monsef Noubadji <67560520+Monsef-Noubadji@users.noreply.github.com>
2022-07-23 18:50:13 +0100

    modified main

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var pwd = document.getElementById("password");
+let pwd = document.getElementById("password");
 function genPwd() { 
-var chars = "1234567890abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPRSTVWXYZ!@#$%^&*()+\][]\';/.,'`` ";
-var pwdLength = 12;
-var pwd = "";
-for (var i = 0; i <= pwdLength ; i++){
-  var RandomPwd = Math.floor(Math.random() * chars.length);
+let chars = "1234567890abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPRSTVWXYZ!@#$%^&*()+\][]\';/.,'`` ";
+let pwdLength = 16
+let pwd = "";
+for (let i = 0; i <= pwdLength ; i++){
+  let RandomPwd = Math.floor(Math.random() * chars.length);
   pwd += chars.substring(RandomPwd , RandomPwd +1);
 }
 document.getElementById("pwd").value = pwd; 
 }
 function copyPwd(){
-  var copyPswd = document.getElementById("pwd");
+  let copyPswd = document.getElementById("pwd");
   copyPswd.select();
   document.execCommand("copy");
   alert("Password Copied successfully !");
